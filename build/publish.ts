91ecd1c1639a442f4b818dcb430bacbd64307245
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2023-03-08 15:21:36 -0300

    errorpublish

--- build/publish.ts
+++ build/publish.ts
@@ -1,7 +1,7 @@
 import { PACKAGES, exec } from './util';
 
 PACKAGES.forEach((name) => {
-  const pkgPath = `${__dirname}/../dist/@ngx-formly/${name}`;
+  const pkgPath = `${__dirname}/../dist/@luizfbicalho/${name}`;
 
   exec(`cd ${pkgPath} && npm publish --access public`);
 });
